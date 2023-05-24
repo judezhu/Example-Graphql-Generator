31194a4b34995b4781ec7b1780baf47035c0ba3d
Innocent Abdullahi <deewai48@gmail.com>
2023-05-24 15:59:11 -0700

    chat completion

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,8 +15,8 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid question",
@@ -26,18 +26,35 @@ export default async function (req, res) {
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: generatePrompt(),
-      temperature: 0,
-      max_tokens: 2000,
-    //   top_p:1,
-    // frequency_penalty:0,
-    // presence_penalty:0,
-    stop:["{}"],
+    let prompt = generatePrompt();
+    console.log(question)
+    console.log(prompt)
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: prompt
+        },
+        {
+          role: 'user',
+          content: question
+        }
+      ],
+      temperature:1,
+      top_p:1,
+      frequency_penalty:0,
+      presence_penalty:0,
     });
+    
+    let message = completion.data.choices[0].message.content
     console.log(completion.data)
-    res.status(200).json({ result: completion.data.choices[0].text });
+    console.log(message)
+    var code = message.substring(
+      message.indexOf("```") + 1, 
+      message.lastIndexOf("```")
+  );
+    res.status(200).json({ result: code });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -58,15 +75,14 @@ function generatePrompt() {
   const fs = require('fs');
 
   try {
-    const data = fs.readFileSync('./schema.graphql', 'utf8');
     const schema_definition = fs.readFileSync('./schema.graphql', 'utf8');
-    return `you are a backend engineer with expertise in graphgl, help me write a graphql request of
-    ###
-    how many moments do andrés iniesta have
-    ###
-    based on the graphql schema below. Please don't include fragement in the returning response:
+    return `You are a backend engineer with expertise in graphgl, 
+    You collect the user's question,
+    and then write the entire graphql request based on the schema listed below. 
+    
+    The graphql schema is:
+
     ${schema_definition}
-    {}
 `;
   } catch (err) {
     console.error(err);
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [questionInput, setQuestionInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ question: questionInput }),
       });
 
       const data = await response.json();
@@ -23,7 +23,7 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setQuestionInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -44,10 +44,10 @@ export default function Home() {
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
+            name="question"
             placeholder="Enter an question"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            value={questionInput}
+            onChange={(e) => setQuestionInput(e.target.value)}
           />
           <input type="submit" value="Generate Graphql" />
         </form>
